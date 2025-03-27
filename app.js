9dc0c2e435e65834fefb25bc1177ca4e79e08e83
Kirath Bhat <kirathuv2009@gmail.com>
2025-03-27 21:56:01 +0530

    Updated Files

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const path = require('path');
 const app = express();
 const port = 80;
 const fs = require('fs');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+
+//Mongoose Specific
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost/contactDance');
+  console.log("Connected to MongoDB");
+}
 
 //PUG Specific
 app.set('view engine', 'pug');
@@ -29,17 +39,23 @@ app.get("/contact", (req, res) => {
     res.status(200).render('contact.pug', params);
 })
 
-//Other
-
-app.post("/contact", (req, res)=>{
-    nam = req.body.name;
-    age = req.body.age;
-    phone = req.body.phone;
-    email = req.body.email;
-    let outputToWrite = `The name of the client is ${nam}, age is ${age} years, phone number is ${phone}. Email is ${email}. \n`;
-    fs.writeFileSync('output.txt', outputToWrite);
-    const params = {'message': 'Your form has been submitted succesfully!'}
-    res.status(200).render('home.pug', params);
-    
-    
+app.post("/contact", (req, res) => {
+    var myData = new Contact(req.body);
+    myData.save().then(()=>{
+        res.render("contact.pug")
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database")
+    });
+    const params = { };
 })
+
+//Mongoose StartUP
+
+const contactSchema = new mongoose.Schema({
+    name: String,
+    phone: String,
+    email: String,
+    age: String
+});
+
+const Contact = mongoose.model('Contact', contactSchema);
